a59346779267dd906bf3324f22ee4a6dfab720f1
ApprenticeofEnder <perfknight@gmail.com>
2021-12-05 17:45:10 -0500

    solve day 1

---
+++ 2021/day-01/ApprenticeofEnder/part1.py
@@ -0,0 +1,8 @@
+with open('part1.txt', 'r') as infile:
+    data = [int(point) for point in infile.readlines()]
+
+increases = 0
+for index, point in enumerate(data):
+    if index == 0: continue
+    if point > data[index - 1]: increases += 1
+print(increases)
