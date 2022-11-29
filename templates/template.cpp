de0ee68cff0b98bec3c192792c1efdc882ac0efd
Nathan Coulas <contact.nathancoulas@gmail.com>
2022-11-29 11:37:00 -0500

    Introduce C++ code template

---
+++ templates/template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+typedef long long LL; 
+typedef pair<int, int> pii; 
+typedef pair<LL, LL> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<LL> vl; 
+typedef vector<vl> vvl;
+typedef queue<int> qi;
+typedef queue<char> qc;
+typedef stack<int> si;
+typedef stack<char> sc;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	string line;
+	while(getline(cin, line)){
+		// perform operations on line, where line is a string representing a line of input
+	}
+
+	return 0;
+}
