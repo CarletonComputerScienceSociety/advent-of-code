1f185001d37ae98086587b98c441511b47500b28
Forest Anderson <forestkzanderson@gmail.com>
2022-11-29 17:00:16 -0500

    Improve template, add more to readme

--- templates/python/main.py
+++ templates/python/main.py
@@ -1,52 +1,25 @@
-def twod_read_input(mylist, file_name):  # returns text from given file in a 2d list
-    f = open(file_name, "r")
-    file_data = f.read()
-    input_text_block = file_data.strip().split("\n")
-    for line in input_text_block:
-        row = []
-        for char in line:
-            row.append(char)
-        mylist.append(row)
-    f.close()
+import sys
 
 
-def oned_read_input(mylist, file_name):  # returns text from given file in a 1d list
-    f = open(file_name, "r")
-    file_data = f.read()
-    input_text_block = file_data.strip().split("\n")
-    for line in input_text_block:
-        for char in line:
-            mylist.append(char)
-    f.close()
+def get_input():
+    # Read input from stdin
+    puzzle_input = sys.stdin.read().strip().split("\n")
+
+    # Do extra parsing here if you need
+    # eg. puzzle_input = puzzle_input[0].split(" ")
+
+    return puzzle_input
 
 
-def line_by_line_input(mylist, file_name):  # returns text line by line in a 1d list
-    f = open(file_name, "r")
-    file_data = f.read()
-    input_text_block = file_data.strip().split("\n")
-    for line in input_text_block:
-        mylist.append(line)
-    f.close()
+def part1(puzzle_input):
+    pass
 
 
-file_name = "input.txt"
+def part2(puzzle_input):
+    pass
 
-twod_list = []
-twod_read_input(twod_list, file_name)
-print("twod_read_input input parsing")
-print(twod_list)
 
-print()
+puzzle_input = get_input()
 
-oned_list = []
-print("oned_read_input input parsing")
-oned_read_input(oned_list, file_name)
-print(oned_list)
-
-print()
-
-line_by_line_list = []
-print("line_by_line_input input parsing")
-
-line_by_line_input(line_by_line_list, file_name)
-print(line_by_line_list)
+print("Part 1: {}".format(part1(puzzle_input)))
+print("Part 2: {}".format(part2(puzzle_input)))
